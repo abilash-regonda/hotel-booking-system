1ff6e6081c5a940b97dec8241f6f58c0709987dd
aregonda <abilash.regonda@sicpa.com>
2023-03-06 11:44:07 +0530

    Created maven project

---
+++ src/main/java/com/sc/hotelbooking/HotelBookingApp.java
@@ -0,0 +1,11 @@
+package com.sc.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+
+public class HotelBookingApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotelBookingApp.class, args);
+  }
+
+}
